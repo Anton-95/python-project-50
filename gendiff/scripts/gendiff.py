b8f18544f30c5d162843ca623e4c6fe072ce2eaf
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-27 21:49:10 +0300

    feat: upgraded help

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,9 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args.echo)
+    print(args.first_file, args.second_file)
     
 if __name__ == '__main__':
     main()
