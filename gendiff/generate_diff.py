5e09f6fb42888935df20a1625f5e2bf38108c309
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-30 20:52:51 +0300

    feature: finding the difference between two json files

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,40 @@
+import json
+
+
+def generate_diff(path_file1, path_file2):
+
+    file1 = json.load(open(path_file1))
+    file2 = json.load(open(path_file2))
+    result_string = ''
+    result_dict = {}
+
+    for key, value in file1.items():
+        value = is_true_or_false(value)
+        if file2.get(key) == file1[key]:
+            result_dict[f'    {key}'] = value
+        elif file2.get(key) is None:
+            result_dict[f'  - {key}'] = value
+        elif file2.get(key) != file1[key] is not None:
+            result_dict[f'  - {key}'] = value
+            result_dict[f'  + {key}'] = file2[key]
+
+    for key, value in file2.items():
+        value = is_true_or_false(value)
+        if file1.get(key) is None:
+            result_dict[f'  + {key}'] = value
+
+    sorted_dict = dict(sorted(
+        result_dict.items(), key=lambda x: (x[0].strip(' -+'), x[0][0])))
+
+    for key, value in sorted_dict.items():
+        result_string += f'{key}: {value}\n'
+
+    return '{' + '\n' + result_string + '}'
+
+
+def is_true_or_false(value):
+    if value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    return value
