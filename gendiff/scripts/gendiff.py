510d7763e7828fd4fabd2f9809c9cdbc0945b8df
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-30 20:50:08 +0300

    added two required arguments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,18 @@
+from gendiff.generate_diff import generate_diff
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args.first_file, args.second_file)
-    
+
+    print(generate_diff(args.first_file, args.second_file))
+
+
 if __name__ == '__main__':
     main()
