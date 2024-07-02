d1a249340b1360cad9665c43981120bc1e3e1897
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-02 10:59:00 +0300

    add pytest fixture

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,18 +1,17 @@
 from gendiff import generate_diff
-
-result_string = '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+import pytest
 
 
-def test_generate_diff():
+@pytest.fixture
+def result_json_string():
+    with open('tests/fixtures/json_string.txt', 'r') as json_string:
+        content = json_string.read()
+        return content
+
+
+def test_generate_diff(result_json_string):
 
     file1 = 'gendiff/file1.json'
     file2 = 'gendiff/file2.json'
 
-    assert generate_diff(file1, file2) == result_string
+    assert generate_diff(file1, file2) == result_json_string
