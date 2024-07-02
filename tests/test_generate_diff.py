a2a14c8fe1085248b884b9f805825b2cfaf0f63c
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-02 16:06:48 +0300

    add tests yaml-files

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,9 +9,17 @@ def result_json_string():
         return content
 
 
-def test_generate_diff(result_json_string):
+def test_generate_diff_json(result_json_string):
 
     file1 = 'gendiff/file1.json'
     file2 = 'gendiff/file2.json'
+
+    assert generate_diff(file1, file2) == result_json_string
+
+
+def test_genetrate_diff_yaml(result_json_string):
+
+    file1 = 'gendiff/file1.yml'
+    file2 = 'gendiff/file2.yml'
 
     assert generate_diff(file1, file2) == result_json_string
