18684111231a2f74eef6fa515de323fa9408c478
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-11 12:28:24 +0300

    fix internal representation diff

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -4,40 +4,36 @@ import yaml
 
 
 def generate_diff(path_file1, path_file2, formatter=stylish):
-    file1, file2 = opened_files(path_file1, path_file2)
+    file1, file2 = opening_files(path_file1, path_file2)
 
     def iter_(file1, file2):
-        result_dict = {}
 
-        for key, value in file1.items():
-            value = is_true_or_false_or_none(value)
-            if not isinstance(value, dict):
-                if file2.get(key) is None:
-                    result_dict[f'- {key}'] = value
-                elif file2.get(key) == file1[key]:
-                    result_dict[f'{key}'] = value
-                elif file2.get(key) != file1[key]:
-                    result_dict[f'- {key}'] = value
-                    result_dict[f'+ {key}'] = file2[key]
-            elif isinstance(value, dict):
-                if isinstance(file2.get(key), dict):
-                    result_dict[key] = iter_(value, file2.get(key))
-                elif not isinstance(file2.get(key), dict):
-                    result_dict[f'- {key}'] = iter_(value, value)
+        keys = file1.keys() | file2.keys()
+        diff_dict = {}
 
-        for key, value in file2.items():
-            value = is_true_or_false_or_none(value)
-            if not isinstance(value, dict):
-                if file1.get(key) is None or file1.get(key) != value:
-                    result_dict[f'+ {key}'] = value
-            elif isinstance(value, dict):
-                if isinstance(file1.get(key), dict):
-                    if file1.get(key) is None:
-                        result_dict[f'+ {key}'] = iter_(value, file1.get(key))
-                elif not isinstance(file1.get(key), dict):
-                    result_dict[f'+ {key}'] = iter_(value, value)
-
-        return result_dict
+        for key in keys:
+            value1 = is_true_or_false_or_none(file1.get(key))
+            value2 = is_true_or_false_or_none(file2.get(key))
+            
+            if not isinstance(value1, dict) and not isinstance(value2, dict):
+                if key not in file1:
+                    diff_dict[key] = (value2, 'added')
+                elif key not in file2:
+                    diff_dict[key] = (value1, 'deleted')
+                elif value1 == value2:
+                    diff_dict[key] = (value1, 'unchanged')
+                else:
+                    diff_dict[key] = (value1, value2, 'changed')
+            elif isinstance(value1, dict) and isinstance(value2, dict):
+                diff_dict[key] = iter_(value1, value2)
+            elif not isinstance(value1, dict) and isinstance(value2, dict) and file1.get(key) is None:
+                diff_dict[key] = (value2, 'added')
+            elif isinstance(value1, dict) and not isinstance(value2, dict) and file2.get(key) is not None:
+                diff_dict[key] = (value1, value2, 'changed')
+            else:
+                diff_dict[key] = (value1, 'deleted')
+            
+        return diff_dict
     return formatter(iter_(file1, file2))
 
 
@@ -51,7 +47,7 @@ def is_true_or_false_or_none(value):
     return value
 
 
-def opened_files(path_file1, path_file2):
+def opening_files(path_file1, path_file2):
     if path_file1.endswith('yaml') or path_file1.endswith('yml'):
         with open(path_file1, 'r') as yml_file:
             file1 = yaml.safe_load(yml_file)
@@ -64,3 +60,34 @@ def opened_files(path_file1, path_file2):
     else:
         file2 = json.load(open(path_file2))
     return file1, file2
+
+# Первый вариант внутреннего представления
+        # diff_dict = {}
+
+        # for key, value in file1.items():
+        #     value = is_true_or_false_or_none(value)
+        #     if not isinstance(value, dict):
+        #         if file2.get(key) is None:
+        #             diff_dict[f'- {key}'] = value
+        #         elif file2.get(key) == file1[key]:
+        #             diff_dict[f'{key}'] = value
+        #         elif file2.get(key) != file1[key]:
+        #             diff_dict[f'- {key}'] = value
+        #             diff_dict[f'+ {key}'] = file2[key]
+        #     elif isinstance(value, dict):
+        #         if isinstance(file2.get(key), dict):
+        #             diff_dict[key] = iter_(value, file2.get(key))
+        #         elif not isinstance(file2.get(key), dict):
+        #             diff_dict[f'- {key}'] = iter_(value, value)
+
+        # for key, value in file2.items():
+        #     value = is_true_or_false_or_none(value)
+        #     if not isinstance(value, dict):
+        #         if file1.get(key) is None or file1.get(key) != value:
+        #             diff_dict[f'+ {key}'] = value
+        #     elif isinstance(value, dict):
+        #         if isinstance(file1.get(key), dict):
+        #             if file1.get(key) is None:
+        #                 diff_dict[f'+ {key}'] = iter_(value, file1.get(key))
+        #         elif not isinstance(file1.get(key), dict):
+        #             diff_dict[f'+ {key}'] = iter_(value, value)
