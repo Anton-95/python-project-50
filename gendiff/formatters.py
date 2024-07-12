98f5abc5a680e5168c7e17be32f61e3e27cdbf5b
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-13 00:13:00 +0300

    fix formatter stylish

--- gendiff/formatters.py
+++ gendiff/formatters.py
@@ -1,13 +1,3 @@
-def is_true_or_false_or_none(value):
-    if value is None:
-        return 'null'
-    elif value is True:
-        return 'true'
-    elif value is False:
-        return 'false'
-    return value
-
-
 def sorting_dict(diff_dict):
     sorted_dict = dict(sorted(
         diff_dict.items(), key=lambda x: (x[0].strip(' -+'), x[0][1])))
@@ -19,78 +9,89 @@ def sorting_dict(diff_dict):
     return sorted_dict
 
 
-def stylish(value, replacer=' ', spaces_count=4, depth=0):
-    if not isinstance(value, dict):
-        return value
-    sorted_dict = sorting_dict(value)
+def stylish(diff, replacer=' ', spaces_count=4, depth=0):
     depth += spaces_count
     result_string = ''
+    if isinstance(diff, list):
+        diff = sorted(diff, key=lambda x: x.get('key'))
 
-    for key, item in sorted_dict.items():
-        if isinstance(item, dict):
-            result_string += f'{depth * replacer}{key}: {stylish(
-                item, replacer, spaces_count, depth)}\n'
+    for item in diff:
 
-        elif not isinstance(item, dict) and not isinstance(item, tuple):
-            result_string += f'{depth * replacer}{key}: {stylish(
-                item, replacer, spaces_count, depth)}\n'
+        if item.get('children') is None:
+            if item.get('status') == 'added':
+                result_string += (
+                    f'{(depth - 2) * replacer}+ '
+                    f'{item['key']}: {item['value']}\n')
+            elif item.get('status') == 'deleted':
+                result_string += (f'{(depth - 2) * replacer}- '
+                                  f'{item['key']}: {item['value']}\n')
+            elif item.get('status') == 'unchanged':
+                result_string += (f'{depth * replacer}'
+                                  f'{item['key']}: {item['value']}\n')
+            elif item.get('status') == 'changed':
+                if not isinstance(item['old_value'], list) and \
+                   not isinstance(item['new_value'], list):
+                    result_string += (f'{(depth - 2) * replacer}- '
+                                      f'{item['key']}: {item['old_value']}\n')
+                    result_string += (f'{(depth - 2) * replacer}+ '
+                                      f'{item['key']}: {item['new_value']}\n')
+                elif isinstance(item['old_value'], list) and \
+                        not isinstance(item['new_value'], list):
+                    result_string += (f'{(depth - 2) * replacer}- '
+                                      f'{item['key']}: '
+                                      f'{stylish(item['old_value'],
+                                                 replacer,
+                                                 spaces_count,
+                                                 depth)}\n')
+                    result_string += (f'{(depth - 2) * replacer}+ '
+                                      f'{item['key']}: {item['new_value']}\n')
+                elif not isinstance(item['old_value'], list) and \
+                        isinstance(item['new_value'], list):
+                    result_string += (f'{(depth - 2) * replacer}- '
+                                      f'{item['key']}: {item['old_value']}\n')
+                    result_string += (f'{(depth - 2) * replacer}+ '
+                                      f'{item['key']}: '
+                                      f'{stylish(item['new_value'],
+                                                 replacer,
+                                                 spaces_count,
+                                                 depth)}\n')
+                elif isinstance(item['old_value'], list) and \
+                        not isinstance(item['new_value'], list):
+                    result_string += (f'{(depth - 2) * replacer}- '
+                                      f'{item['key']}: '
+                                      f'{stylish(item['old_value'],
+                                                 replacer,
+                                                 spaces_count,
+                                                 depth)}\n')
+                    result_string += (f'{(depth - 2) * replacer}+ '
+                                      f'{item['key']}: '
+                                      f'{stylish(item['new_value'],
+                                                 replacer,
+                                                 spaces_count,
+                                                 depth)}\n')
 
-        elif isinstance(item, tuple):
+        elif item['children'] is not None:
+            if item.get('status') == 'added':
+                result_string += (f'{(depth - 2) * replacer}+ '
+                                  f'{item['key']}: '
+                                  f'{stylish(item['children'],
+                                             replacer,
+                                             spaces_count,
+                                             depth)}\n')
+            elif item.get('status') == 'deleted':
+                result_string += (f'{(depth - 2) * replacer}- '
+                                  f'{item['key']}: '
+                                  f'{stylish(item['children'],
+                                             replacer,
+                                             spaces_count,
+                                             depth)}\n')
+            elif item.get('status') == 'unchanged':
+                result_string += (f'{depth * replacer}'
+                                  f'{item['key']}: '
+                                  f'{stylish(item['children'],
+                                             replacer,
+                                             spaces_count,
+                                             depth)}\n')
 
-            if item[-1] == 'added' and isinstance(item[0], dict):
-                result_string += f'{(depth - 2) * replacer}+ {key}: {stylish(
-                    item[0], replacer, spaces_count, depth)}\n'
-            elif item[-1] == 'added' and not isinstance(item[0], dict):
-                result_string += f'{(depth - 2) * replacer}+ {key}: {item[0]}\n'
-
-            elif item[-1] == 'deleted' and isinstance(item[0], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {stylish(
-                    item[0], replacer, spaces_count, depth)}\n'
-            elif item[-1] == 'deleted' and not isinstance(item[0], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {item[0]}\n'
-
-            elif item[-1] == 'unchanged' and isinstance(item[0], dict):
-                result_string += f'{depth * replacer}{key}: {stylish(
-                    item[0], replacer, spaces_count, depth)}\n'
-            elif item[-1] == 'unchanged' and not isinstance(item[0], dict):
-                result_string += f'{depth * replacer}{key}: {item[0]}\n'
-
-            elif item[-1] == 'changed' and isinstance(item[0], dict) and isinstance(item[1], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {stylish(
-                    item[0], replacer, spaces_count, depth)}\n'
-                result_string += f'{(depth - 2) * replacer}+ {key}: {stylish(
-                    item[1], replacer, spaces_count, depth)}\n'
-            elif item[-1] == 'changed' and not isinstance(item[0], dict) and not isinstance(item[1], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {item[0]}\n'
-                result_string += f'{(depth - 2) * replacer}+ {key}: {item[1]}\n'
-            elif item[-1] == 'changed' and isinstance(item[0], dict) and not isinstance(item[1], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {stylish(
-                    item[0], replacer, spaces_count, depth)}\n'
-                result_string += f'{(depth - 2) * replacer}+ {key}: {item[1]}\n'
-            elif item[-1] == 'changed' and not isinstance(item[0], dict) and isinstance(item[1], dict):
-                result_string += f'{(depth - 2) * replacer}- {key}: {item[0]}\n'
-                result_string += f'{(depth - 2) * replacer}+ {key}: {stylish(
-                    item[1], replacer, spaces_count, depth)}\n'
-
-    result = '{\n' + result_string + (replacer * (
-        depth - spaces_count)) + '}\n'
+    result = '{\n' + result_string + (replacer * (depth - spaces_count)) + '}\n'
     return result.strip('\n')
-
-# stylish для первого варианта внутреннего представления дифа
-    # for key, item in sorted_dict.items():
-    #     if str(key).startswith('+') or str(key).startswith('-'):
-    #         result_string += f'{(depth - 2) * replacer}{key}: {stylish(
-    #             item, replacer, spaces_count, depth)}\n'
-    #     else:
-    #         result_string += f'{depth * replacer}{key}: {stylish(
-    #             item, replacer, spaces_count, depth)}\n'
-
-# def plain(value):
-#     if not isinstance(value, dict):
-#         return value
-#     sorted_dict = sorting_dict(value)
-    
-#     for key, item in sorted_dict.items():
-#         if isinstance(item, tuple):
-#             if item[-1] == 'added':
-                
