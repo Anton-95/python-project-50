b8ee3043de2b00df2eb75c0f1216f67f2ba2ca63
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-23 22:01:00 +0300

    fix output json formatter

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,10 +1,18 @@
 import json
 
 
-def gen_json_format(diff):
+def sorting_diff(diff):
     if isinstance(diff, list):
-        return sorted([gen_json_format(elem) for elem in diff],
+        return sorted([sorting_diff(elem) for elem in diff],
                       key=lambda x: x['key'])
     if isinstance(diff, dict):
-        return {k: gen_json_format(v) for k, v in sorted(diff.items())}
-    return json.dumps(diff)
+        return {k: sorting_diff(v) for k, v in sorted(diff.items())}
+    return diff
+
+
+def gen_json_format(diff):
+    sorted_diff = sorting_diff(diff)
+    result_string = ''
+    for value in sorted_diff:
+        result_string += f'{value}, '
+    return json.dumps(result_string.strip(', '))
