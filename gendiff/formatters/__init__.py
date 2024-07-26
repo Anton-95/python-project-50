269e194484c8a1c48da991ba933d0e6df1090231
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-26 22:08:16 +0300

    separating format selection function into a separate module

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -2,6 +2,16 @@ from gendiff.formatters.plain import gen_plain_format as plain
 from gendiff.formatters.stylish import gen_nested_format as stylish
 from gendiff.formatters.json import gen_json_format as json
 
+
+def format_selection(diff, formatter):
+    if formatter == 'plain':
+        return plain(diff)
+    elif formatter == 'json':
+        return json(diff)
+    elif formatter == 'stylish':
+        return stylish(diff)
+
+
 __all__ = (
     'plain',
     'stylish',
