9dbc1830a91bd9fbb00bfbc52bd451d3ae82ddb2
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-07-28 18:06:15 +0300

    mod formatters

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,18 +1,8 @@
 import json
 
 
-def sorting_diff(diff):
-    if isinstance(diff, list):
-        return sorted([sorting_diff(elem) for elem in diff],
-                      key=lambda x: x['key'])
-    if isinstance(diff, dict):
-        return {k: sorting_diff(v) for k, v in sorted(diff.items())}
-    return diff
-
-
 def gen_json_format(diff):
-    sorted_diff = sorting_diff(diff)
     result_string = ''
-    for value in sorted_diff:
+    for value in diff:
         result_string += f'{value}, '
     return json.dumps(result_string.strip(', '))
--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,48 +1,56 @@
 def gen_plain_format(diff, path=''):
     result_string = ''
-    if isinstance(diff, list):
-        diff = sorted(diff, key=lambda x: x.get('key'))
 
     for value in diff:
 
-        if value.get('children') is None:
+        if value.get('status') != 'nested':
             path += f'{value['key']}.'
-            if value['status'] == 'added':
+
+            if value.get('status') is None:
+                path = path[0:-len(value['key']) - 1]
+
+            elif value['status'] == 'added':
                 result_string += (f"Property '{path.strip('.')}' "
                                   f"was added with value: "
-                                  f"{is_bool_or_list(value['value'])}\n")
+                                  f"{value_convers(value['value'])}\n")
                 path = path[0:-len(value['key']) - 1]
+
             elif value['status'] == 'deleted':
                 result_string += f"Property '{path.strip('.')}' was removed\n"
                 path = path[0:-len(value['key']) - 1]
+
             elif value['status'] == 'changed':
                 result_string += (f"Property '{path.strip('.')}' was updated. "
-                                  f"From {is_bool_or_list(value['old_value'])} "
-                                  f"to {is_bool_or_list(value['new_value'])}\n")
-                path = path[0:-len(value['key']) - 1]
-            elif value['status'] == 'unchanged':
+                                  f"From {value_convers(value['old_value'])} "
+                                  f"to {value_convers(value['new_value'])}\n")
                 path = path[0:-len(value['key']) - 1]
 
-        else:
+        elif value.get('status') == 'nested':
             path += f'{value['key']}.'
+
             if value['status'] == 'added':
                 result_string += (f"Property '{path.strip('.')}' "
                                   f"was added with value: [complex value]")
+
             elif value['status'] == 'deleted':
                 result_string += f"Property '{path.strip('.')}' was removed"
                 path = path[0:-len(value.get('key')) - 1]
-            result_string += (f'{gen_plain_format(value.get('children'),
+            result_string += (f'{gen_plain_format(value.get('value'),
                               path)}\n')
             path = path[0:-len(value.get('key')) - 1]
 
     return result_string.strip('\n')
 
 
-def is_bool_or_list(value):
+def value_convers(value):
     if isinstance(value, list):
         return '[complex value]'
-    elif value == 'true' or value == 'false' or value == 'null':
-        return f'{value}'
+    elif value is None:
+        return 'null'
+    elif value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
     elif isinstance(value, int):
         return value
     else:
