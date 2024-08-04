1eb62957b07010c2cc7720ffa482010000b286e9
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-04 18:37:45 +0300

    mod iteration func

--- gendiff/iteration.py
+++ gendiff/iteration.py
@@ -16,43 +16,68 @@ def iteration(file1, file2):
 
         else:
             if key not in file1:
-                if isinstance(value2, dict):
-                    diff += [{'key': key,
-                              'value': iteration(value2, value2),
-                              'status': 'added'}]
-                else:
-                    diff += [{'key': key,
-                              'value': value2,
-                              'status': 'added'}]
+                diff += [{'key': key,
+                          'value': value2,
+                          'status': 'added'}]
 
             elif key not in file2:
-                if isinstance(value1, dict):
-                    diff += [{'key': key,
-                              'value': iteration(value1, value1),
-                              'status': 'deleted'}]
-                else:
-                    diff += [{'key': key,
-                              'value': value1,
-                              'status': 'deleted'}]
+                diff += [{'key': key,
+                          'value': value1,
+                          'status': 'deleted'}]
 
             elif value1 != value2:
-                if isinstance(value1, dict):
-                    diff += [{'key': key,
-                              'old_value': iteration(value1, value1),
-                              'new_value': value2,
-                              'status': 'changed'}]
-                elif isinstance(value2, dict):
-                    diff += [{'key': key,
-                              'old_value': value1,
-                              'new_value': iteration(value2, value2),
-                              'status': 'changed'}]
-                else:
-                    diff += [{'key': key,
-                              'old_value': value1,
-                              'new_value': value2,
-                              'status': 'changed'}]
+                diff += [{'key': key,
+                          'old_value': value1,
+                          'new_value': value2,
+                          'status': 'changed'}]
             else:
                 diff += [{'key': key,
                           'value': value1}]
+
+        # if isinstance(value1, dict) and isinstance(value2, dict):
+        #     diff += [{'key': key,
+        #               'value': iteration(value1, value2),
+        #               'status': 'nested'}]
+
+        # else:
+        #     if key not in file1:
+        #         if isinstance(value2, dict):
+        #             diff += [{'key': key,
+        #                       'value': iteration(value2, value2),
+        #                       'status': 'added'}]
+        #         else:
+        #             diff += [{'key': key,
+        #                       'value': value2,
+        #                       'status': 'added'}]
+
+        #     elif key not in file2:
+        #         if isinstance(value1, dict):
+        #             diff += [{'key': key,
+        #                       'value': iteration(value1, value1),
+        #                       'status': 'deleted'}]
+        #         else:
+        #             diff += [{'key': key,
+        #                       'value': value1,
+        #                       'status': 'deleted'}]
+
+        #     elif value1 != value2:
+        #         if isinstance(value1, dict):
+        #             diff += [{'key': key,
+        #                       'old_value': iteration(value1, value1),
+        #                       'new_value': value2,
+        #                       'status': 'changed'}]
+        #         elif isinstance(value2, dict):
+        #             diff += [{'key': key,
+        #                       'old_value': value1,
+        #                       'new_value': iteration(value2, value2),
+        #                       'status': 'changed'}]
+        #         else:
+        #             diff += [{'key': key,
+        #                       'old_value': value1,
+        #                       'new_value': value2,
+        #                       'status': 'changed'}]
+        #     else:
+        #         diff += [{'key': key,
+        #                   'value': value1}]
 
     return diff
