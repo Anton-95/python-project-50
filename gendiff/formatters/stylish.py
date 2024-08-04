4f03b9c5b9c3e5e0e150c534a1b83970841e2bd7
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-04 18:39:22 +0300

    fix stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -6,13 +6,13 @@ def gen_nested_format(diff, replacer=' ', spaces_count=4, depth=0):
 
         if value.get('status') != 'nested':
             if value.get('status') == 'added':
-                if isinstance(value['value'], list):
-                    result_string += (
-                        f'{(depth - 2) * replacer}+ '
-                        f'{value['key']}: {gen_nested_format(value['value'],
-                                                             replacer,
-                                                             spaces_count,
-                                                             depth)}\n')
+                if isinstance(value['value'], dict):
+                    result_string += (f'{(depth - 2) * replacer}+ '
+                                      f'{value['key']}: '
+                                      f'{iteration_dict(value['value'],
+                                                        depth,
+                                                        spaces_count,
+                                                        replacer)}\n')
                 else:
                     value['value'] = to_str(value['value'])
                     result_string += (
@@ -20,34 +20,34 @@ def gen_nested_format(diff, replacer=' ', spaces_count=4, depth=0):
                         f'{value['key']}: {value['value']}\n')
 
             elif value.get('status') == 'deleted':
-                if isinstance(value['value'], list):
+                if isinstance(value['value'], dict):
                     result_string += (f'{(depth - 2) * replacer}- '
                                       f'{value['key']}: '
-                                      f'{gen_nested_format(value['value'],
-                                                           replacer,
-                                                           spaces_count,
-                                                           depth)}\n')
+                                      f'{iteration_dict(value['value'],
+                                                        depth,
+                                                        spaces_count,
+                                                        replacer)}\n')
                 else:
                     value['value'] = to_str(value['value'])
                     result_string += (f'{(depth - 2) * replacer}- '
                                       f'{value['key']}: {value['value']}\n')
 
             elif value.get('status') is None:
-                if isinstance(value['value'], list):
-                    result_string += (f'{depth * replacer}'
+                if isinstance(value['value'], dict):
+                    result_string += (f'{(depth - 2) * replacer}+ '
                                       f'{value['key']}: '
-                                      f'{gen_nested_format(value['value'],
-                                                           replacer,
-                                                           spaces_count,
-                                                           depth)}\n')
+                                      f'{iteration_dict(value['value'],
+                                                        depth,
+                                                        spaces_count,
+                                                        replacer)}\n')
                 else:
                     value['value'] = to_str(value['value'])
                     result_string += (f'{depth * replacer}'
                                       f'{value['key']}: {value['value']}\n')
 
             elif value.get('status') == 'changed':
-                if not isinstance(value['old_value'], list) and \
-                   not isinstance(value['new_value'], list):
+                if not isinstance(value['old_value'], dict) and \
+                   not isinstance(value['new_value'], dict):
                     value['old_value'] = to_str(value['old_value'])
                     value['new_value'] = to_str(value['new_value'])
                     result_string += (f'{(depth - 2) * replacer}- '
@@ -55,29 +55,29 @@ def gen_nested_format(diff, replacer=' ', spaces_count=4, depth=0):
                     result_string += (f'{(depth - 2) * replacer}+ '
                                       f'{value['key']}: {value['new_value']}\n')
 
-                elif isinstance(value['old_value'], list) and \
-                        not isinstance(value['new_value'], list):
+                elif isinstance(value['old_value'], dict) and \
+                        not isinstance(value['new_value'], dict):
                     value['new_value'] = to_str(value['new_value'])
                     result_string += (f'{(depth - 2) * replacer}- '
                                       f'{value['key']}: '
-                                      f'{gen_nested_format(value['old_value'],
-                                         replacer,
-                                         spaces_count,
-                                         depth)}\n')
+                                      f'{iteration_dict(value['old_value'],
+                                                        depth,
+                                                        spaces_count,
+                                                        replacer)}\n')
                     result_string += (f'{(depth - 2) * replacer}+ '
                                       f'{value['key']}: {value['new_value']}\n')
 
-                elif not isinstance(value['old_value'], list) and \
-                        isinstance(value['new_value'], list):
+                elif not isinstance(value['old_value'], dict) and \
+                        isinstance(value['new_value'], dict):
                     value['old_value'] = to_str(value['old_value'])
                     result_string += (f'{(depth - 2) * replacer}- '
                                       f'{value['key']}: {value['old_value']}\n')
-                    result_string += (f'{(depth - 2) * replacer}+ '
+                    result_string += (f'{(depth - 2) * replacer}- '
                                       f'{value['key']}: '
-                                      f'{gen_nested_format(value['new_value'],
-                                         replacer,
-                                         spaces_count,
-                                         depth)}\n')
+                                      f'{iteration_dict(value['new_value'],
+                                                        depth,
+                                                        spaces_count,
+                                                        replacer)}\n')
 
         if value.get('status') == 'nested':
             result_string += (f'{depth * replacer}'
@@ -86,6 +86,25 @@ def gen_nested_format(diff, replacer=' ', spaces_count=4, depth=0):
                                  replacer,
                                  spaces_count,
                                  depth)}\n')
+
+    result = '{\n' + result_string + (replacer * (depth - spaces_count)) + '}\n'
+    return result.strip('\n')
+
+
+def iteration_dict(diff, depth, spaces_count, replacer):
+    depth += spaces_count
+    result_string = ''
+
+    for key, value in diff.items():
+        if not isinstance(value, dict):
+            result_string += (f'{depth * replacer}'
+                              f'{key}: {value}\n')
+        elif isinstance(value, dict):
+            result_string += (f'{depth * replacer}'
+                              f'{key}: {iteration_dict(value,
+                                                       depth,
+                                                       spaces_count,
+                                                       replacer)}\n')
 
     result = '{\n' + result_string + (replacer * (depth - spaces_count)) + '}\n'
     return result.strip('\n')
