709aefb822169aa096fb7519bba96c77c9a91850
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-06 22:18:30 +0300

    mod func plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,43 +1,37 @@
 def gen_plain_format(diff, path=''):
     result_string = ''
 
+    def del_last_path(path, del_path):
+        return path[0:-len(del_path['key']) - 1]
+
     for value in diff:
 
         if value.get('status') != 'nested':
             path += f'{value['key']}.'
 
             if value.get('status') is None:
-                path = path[0:-len(value['key']) - 1]
+                path = del_last_path(path, value)
 
             elif value['status'] == 'added':
                 result_string += (f"Property '{path.strip('.')}' "
                                   f"was added with value: "
                                   f"{to_str(value['value'])}\n")
-                path = path[0:-len(value['key']) - 1]
+                path = del_last_path(path, value)
 
             elif value['status'] == 'deleted':
                 result_string += f"Property '{path.strip('.')}' was removed\n"
-                path = path[0:-len(value['key']) - 1]
+                path = del_last_path(path, value)
 
             elif value['status'] == 'changed':
                 result_string += (f"Property '{path.strip('.')}' was updated. "
                                   f"From {to_str(value['old_value'])} "
                                   f"to {to_str(value['new_value'])}\n")
-                path = path[0:-len(value['key']) - 1]
+                path = del_last_path(path, value)
 
         elif value.get('status') == 'nested':
             path += f'{value['key']}.'
-
-            if value['status'] == 'added':
-                result_string += (f"Property '{path.strip('.')}' "
-                                  f"was added with value: [complex value]")
-
-            elif value['status'] == 'deleted':
-                result_string += f"Property '{path.strip('.')}' was removed"
-                path = path[0:-len(value.get('key')) - 1]
-            result_string += (f'{gen_plain_format(value.get('value'),
-                              path)}\n')
-            path = path[0:-len(value.get('key')) - 1]
+            result_string += f'{gen_plain_format(value.get('value'), path)}\n'
+            path = del_last_path(path, value)
 
     return result_string.strip('\n')
 
