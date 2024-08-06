9846bc0915e9729b4eab910959761715e23654e7
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-06 20:56:04 +0300

    mod func

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -10,28 +10,28 @@ def iteration(file1, file2):
         value2 = file2.get(key)
 
         if isinstance(value1, dict) and isinstance(value2, dict):
-            diff += [{'key': key,
-                      'value': iteration(value1, value2),
-                      'status': 'nested'}]
+            diff.append({'key': key,
+                         'value': iteration(value1, value2),
+                         'status': 'nested'})
 
         else:
             if key not in file1:
-                diff += [{'key': key,
-                          'value': value2,
-                          'status': 'added'}]
+                diff.append({'key': key,
+                             'value': value2,
+                             'status': 'added'})
 
             elif key not in file2:
-                diff += [{'key': key,
-                          'value': value1,
-                          'status': 'deleted'}]
+                diff.append({'key': key,
+                             'value': value1,
+                             'status': 'deleted'})
 
             elif value1 != value2:
-                diff += [{'key': key,
-                          'old_value': value1,
-                          'new_value': value2,
-                          'status': 'changed'}]
+                diff.append({'key': key,
+                             'old_value': value1,
+                             'new_value': value2,
+                             'status': 'changed'})
             else:
-                diff += [{'key': key,
-                          'value': value1}]
+                diff.append({'key': key,
+                             'value': value1})
 
     return diff
