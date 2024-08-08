c80c709f2bf826db68a8ed6103fce6d7a8d7f1b1
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-08 22:25:48 +0300

    fix json formatter

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -2,7 +2,4 @@ import json
 
 
 def gen_json_format(diff):
-    result_string = ''
-    for value in diff:
-        result_string += f'{value}, '
-    return json.dumps(result_string.strip(', '))
+    return json.dumps(diff)
