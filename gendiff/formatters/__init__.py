f3f7926145d884f3ba1ceb49c52de41c2ca706d1
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-12 19:19:17 +0300

    renaming a variable

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,12 +1,12 @@
-from gendiff.formatters.plain import gen_plain_format
-from gendiff.formatters.stylish import gen_nested_format
 from gendiff.formatters.json import gen_json_format
+from gendiff.formatters.plain import gen_plain_format
+from gendiff.formatters.stylish import gen_stylish_format
 
 
 def get_formatter(formatter):
-    if formatter == 'plain':
+    if formatter == "plain":
         return gen_plain_format
-    elif formatter == 'json':
+    elif formatter == "json":
         return gen_json_format
-    elif formatter == 'stylish':
-        return gen_nested_format
+    elif formatter == "stylish":
+        return gen_stylish_format
--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -5,5 +5,5 @@ from gendiff.diff_builder import builder
 
 def generate_diff(path_file1, path_file2, formatter='stylish'):
     file1, file2 = load_file(path_file1), load_file(path_file2)
-    format = get_formatter(formatter)
-    return format(builder(file1, file2))
+    formatter = get_formatter(formatter)
+    return formatter(builder(file1, file2))
