d5f59ff974b73d08f91f05feb8233136704b839b
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-12 22:14:13 +0300

    refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -14,36 +14,36 @@ def read_result(path_file):
             "tests/fixtures/flat_file1.json",
             "tests/fixtures/flat_file2.yml",
             "stylish",
-            read_result("tests/fixtures/stylish_flat_string.txt"),
+            "tests/fixtures/stylish_flat_string.txt"
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "stylish",
-            read_result("tests/fixtures/stylish_nested_string.txt"),
+            "tests/fixtures/stylish_nested_string.txt"
         ),
         (
             "tests/fixtures/flat_file1.json",
             "tests/fixtures/flat_file2.yml",
             "plain",
-            read_result("tests/fixtures/plain_flat_string.txt"),
+            "tests/fixtures/plain_flat_string.txt"
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "plain",
-            read_result("tests/fixtures/plain_nested_string.txt"),
+            "tests/fixtures/plain_nested_string.txt"
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "json",
-            read_result("tests/fixtures/json_string.txt"),
+            "tests/fixtures/json_string.txt"
         ),
     ],
 )
 def test_generate_diff_formatters(file1, file2, formatter, result):
-    assert generate_diff(file1, file2, formatter) == result
+    assert generate_diff(file1, file2, formatter) == read_result(result)
 
 
 @pytest.mark.parametrize(
@@ -52,7 +52,7 @@ def test_generate_diff_formatters(file1, file2, formatter, result):
         (
             "tests/fixtures/flat_file1.txt",
             "tests/fixtures/flat_file2.yml",
-            "stylish",
+            "stylish"
         ),
     ]
 )
@@ -69,7 +69,7 @@ def test_invalid_format_file(file1, file2, formatter):
         (
             "tests/fixtures/flat_file1.json",
             "tests/fixtures/flat_file2.yml",
-            "nested",
+            "nested"
         ),
     ],
 )
