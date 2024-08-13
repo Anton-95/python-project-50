5e2d93049241e6a1ce3f499f80409c85039b17aa
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-08-13 19:58:07 +0300

    refactoring tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -14,31 +14,31 @@ def read_result(path_file):
             "tests/fixtures/flat_file1.json",
             "tests/fixtures/flat_file2.yml",
             "stylish",
-            "tests/fixtures/stylish_flat_string.txt"
+            "tests/fixtures/stylish_flat_string.txt",
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "stylish",
-            "tests/fixtures/stylish_nested_string.txt"
+            "tests/fixtures/stylish_nested_string.txt",
         ),
         (
             "tests/fixtures/flat_file1.json",
             "tests/fixtures/flat_file2.yml",
             "plain",
-            "tests/fixtures/plain_flat_string.txt"
+            "tests/fixtures/plain_flat_string.txt",
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "plain",
-            "tests/fixtures/plain_nested_string.txt"
+            "tests/fixtures/plain_nested_string.txt",
         ),
         (
             "tests/fixtures/nested_file3.json",
             "tests/fixtures/nested_file4.yml",
             "json",
-            "tests/fixtures/json_string.txt"
+            "tests/fixtures/json_string.txt",
         ),
     ],
 )
@@ -46,36 +46,26 @@ def test_generate_diff_formatters(file1, file2, formatter, result):
     assert generate_diff(file1, file2, formatter) == read_result(result)
 
 
-@pytest.mark.parametrize(
-    "file1, file2, formatter",
-    [
-        (
+def test_invalid_format_file():
+    with pytest.raises(
+        ValueError,
+        match=("Unsupported file format. "
+               "Expected '.yaml', '.yml' or '.json'."),
+    ):
+        generate_diff(
             "tests/fixtures/flat_file1.txt",
             "tests/fixtures/flat_file2.yml",
             "stylish"
-        ),
-    ]
-)
-def test_invalid_format_file(file1, file2, formatter):
-    with pytest.raises(ValueError,
-                       match=("Unsupported file format. "
-                              "Expected '.yaml', '.yml' or '.json'.")):
-        generate_diff(file1, file2, formatter)
+        )
 
 
-@pytest.mark.parametrize(
-    "file1, file2, formatter",
-    [
-        (
-            "tests/fixtures/flat_file1.json",
-            "tests/fixtures/flat_file2.yml",
-            "nested"
-        ),
-    ],
-)
-def test_invalid_formatter(file1, file2, formatter):
+def test_invalid_formatter():
     with pytest.raises(
         ValueError,
         match=("Unsupported format. Expected 'stylish', 'plain' or 'json'"),
     ):
-        generate_diff(file1, file2, formatter)
+        generate_diff(
+            "tests/fixtures/flat_file1.json",
+            "tests/fixtures/flat_file2.yml",
+            "nested"
+        )
